551d75d01a3b17359b8f84ca8bd9267cc639be57
Richa17goyal <richa175goyal@gmail.com>
2024-08-28 10:58:05 +0100

    authentication to user is updated via login_required

--- chaiheadq/tweet/views.py
+++ chaiheadq/tweet/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Tweet
 from .forms import TweetForm
 from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def index(request):
@@ -13,7 +14,7 @@ def tweet_list(request):
     return render(request, 'tweet_list.html', {'tweets': tweets})
 
 # CREATING TWEET
-
+@login_required
 def tweet_create(request):
     if request.method == "POST":
         form = TweetForm(request.POST, request.FILES)
@@ -26,7 +27,7 @@ def tweet_create(request):
        form = TweetForm()
     return render(request, 'tweet_form.html', {'form': form})
 
-
+@login_required
 def tweet_edit(request, tweet_id):
     tweet = get_object_or_404(Tweet, pk=tweet_id, user = request.user)
     if request.method == "POST":
@@ -40,7 +41,7 @@ def tweet_edit(request, tweet_id):
       form = TweetForm(instance=tweet)
     return render(request, 'tweet_form.html', {'form': form})
 
-
+@login_required
 def tweet_delete(request, tweet_id):
    tweet = get_object_or_404(Tweet, pk=tweet_id, user = request.user)
    if request.method == "POST":
